75a6bca279f98abddfe724fe7ac828c472e12154
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-02-08 12:02:30 -0800

    Cleaning of create-react-app done. Adding in basic structure.

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "../../Assets/scss/main.scss"
+import './Form.scss';
+
+function Form() {
+
+  // Do stuff 
+  return (
+    <div className="Form">
+      This is a form
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "../../Assets/scss/main.scss"
+import './Header.scss';
+
+function Header() {
+
+  // Do stuff 
+  return (
+    <div className="Header">
+      This is a header
+    </div>
+  );
+}
+
+export default Header;
