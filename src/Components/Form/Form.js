2bb61d5e6eae1791d0122d28838f7a1c9dfd49d0
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-02-09 12:35:15 -0800

    Fixed the form. Should be ready to hook up to the API.

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -17,15 +17,8 @@ import { submitForm, resetForm } from "../../Store/form";
 const mapDispatchToProps = { submitForm, resetForm };
 
 function Form(props) {
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    let formInfo = event.target.elements;
-    let sendFormData = {
-      name: formInfo.textTest.value
-    }
-    props.submitForm(sendFormData);
-  }
+  const [isDeployed, setIsDeployed] = React.useState(false);
+  const [isApproved, setIsApproved] = React.useState(false);
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -40,8 +33,8 @@ function Form(props) {
       margin: theme.spacing(0.5),
     },
   }));
-
   const classes = useStyles();
+
   const [chipData, setChipData] = React.useState([
     { key: 0, label: 'React' },
     { key: 1, label: 'Angular' },
@@ -51,130 +44,217 @@ function Form(props) {
     { key: 5, label: 'NodeJS' },
     { key: 6, label: 'Mongo DB' },
     { key: 7, label: 'Material UI' },
-
   ]);
 
+  const handleDeployedChange = (event) => {
+    //console.log(event.target.value);
+    let radioState = event.target.value === 'true' ? true : false
+    setIsDeployed(radioState);
+  };
+  const handleApprovedChange = (event) => {
+    //console.log(event.target.value);
+    let radioState = event.target.value === 'true' ? true : false
+    setIsApproved(radioState);
+  };
 
+  useEffect(() => {
+    console.log(`isApproved was changed to:`, isApproved);
+  }, [isApproved]);
 
-  const handleSubmit =(e)=>{
-    e.preventDefault();
-    let formInfo = e.target.elements;
-    // console.log('Project Name:....', formInfo.projectName.value)
-    // console.log('description:....', formInfo.description.value)
-    // console.log('Class level value:....', formInfo.classLevel.value)
-    // console.log('Class Code:....', formInfo.classCode.value)
-    // console.log('Live URL:....', formInfo.isLiveUrl.value)
-    // console.log('Project Tags:....', formInfo.projectTags.value)
-    
-    console.log('Looking for Radio:....', formInfo.projectTags[3].defaultChecked)
-    console.log('Looking for Radio:....', formInfo.projectTags[3].checked)
-    console.log('Looking for Radio:....', formInfo.projectTags[3].value)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formInfo = event.target.elements;
+    console.log(formInfo);
+    // Need handling for authors. Convert CSL to array.
+    let authorArr = [];
+    formInfo.projectAuthors.value.split(',').forEach(t => { authorArr.push(t.trim())});
+    // Need handling for tags. Convert CSL to array.
+    let tagArr = [];
+    formInfo.projectTags.value.split(',').forEach(t => { tagArr.push(t.trim())});
+    // Need handling for upvotedBy. Convert CSL to array.
+    let upvotedByArr = [];
+    formInfo.projectUpvotedBy.value.split(',').forEach(t => { upvotedByArr.push(t.trim())});
 
-    // foreach input, if projectTags.checked is true, push projectTags.value into an array
-
-
+    let sendFormData = {
+      "projectName": formInfo.projectName.value,
+      "authors": authorArr,
+      "description": formInfo.projectDescription.value,
+      "image": formInfo.projectImages.value,
+      "productionDate": formInfo.projectDate.value,
+      "classCode": formInfo.projectDescription.value,
+      "courseLevel": formInfo.projectClassLevel.value,
+      "githubRepo": formInfo.projectGithubUrl.value,
+      "isLiveStatus": isDeployed,
+      "isLiveUrl": formInfo.projectDeploymentUrl.value, // Not Required
+      "tags": tagArr,                                   // Not Required
+      "postedBy": formInfo.projectPostedBy.value,       // Required, not shown, attach the user's id
+      "upvotedBy": upvotedByArr,                        // Shouldn't be set by the front end
+      "approved": isApproved, // Shouldn't be set by the front end
+      "upvotes": 0,                                     // Shouldn't be set by the front end
+    }
+    props.submitForm(sendFormData);
   }
 
-  // Do stuff 
+  console.log("Props for formData", props.form.formData);
+
   return (
     <div className="Form">
-      <form onSubmit={handleSubmit}>
-        <label for="textTest">Text Test:
-          <input id="textTest" name="textTest" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      <pre>formData:
-      {props.form.formData.map((data, idx) => (
-        <p key={idx}>{idx}: {data.name}</p>
+      {/* <pre>formData:
+      {Object.keys(props.form.formData).map((data, idx) => (
+        <pre key={idx}><strong>{data}</strong>: {props.form.formData[data]}</pre>
       ))}
-      </pre>
-
+      </pre> */}
+      <h2>
+        Project Submission
+      </h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Project Submission
-        </label>
-        <br />
-
         <label for="projectName">
           Project Name:
-          <br />
-          <input id="projectName" name="projectName" />
-
         </label>
         <br />
-        <label for="description">
+        <input id="projectName" name="projectName" required />
+        <br />
+
+        <label for="projectAuthors">
+          Authors (Comma Separated):
+        </label>
+        <br />
+        <input id="projectAuthors" name="projectAuthors" required />
+        <br />
+
+        <label for="projectDescription">
           Description:
-          <br />
-          <textarea id="description" name="description" />
-
         </label>
         <br />
-        <label for="classLevel">
+        <textarea id="projectDescription" name="projectDescription" required />
+        <br />
+
+        <label for="projectImages">
+          Select project images:
+        </label>
+        <br />
+        <input type="file" id="projectImages" name="projectImages" multiple required />
+        <br />
+
+        <label for="projectDate">
+          Production Date:
+        </label>
+        <br />
+        <input type="date" id="projectDate" name="projectDate" required />
+        <br />
+
+        <label for="projectClassLevel">
           Class Level:
-          <br />
-          <select id="classLevel" name="classLevel">
-            <option value="101">101</option>
-            <option value="102">102</option>
-            <option value="201">201</option>
-            <option value="301">301</option>
-            <option value="401JavaScript">401 JavaScript</option>
-            <option value="401DotNet">401 .Net</option>
-            <option value="401Java">401 Java</option>
-            <option value="401Python">401 Python</option>
-
-          </select>
         </label>
         <br />
-        <label for="classCode">
+        <select id="projectClassLevel" name="projectClassLevel" required>
+          <optgroup label="Software Development">
+            <option value="SD100">100: Beginner</option>
+            <option value="SD200">200: Foundations</option>
+            <option value="SD300">300: Intermediate</option>
+            <option value="SD400">400: Advanced</option>
+            <option value="SD500">500: Continuing Education</option>
+          </optgroup>
+          <optgroup label="Ops and Cybersecurity">
+            <option value="OC100">100: Beginner</option>
+            <option value="OC200">200: Foundations</option>
+            <option value="OC300">300: Intermediate</option>
+            <option value="OC400">400: Advanced</option>
+          </optgroup>
+        </select>
+        <br />
+
+        <label for="projectClassCode">
           Class Code:
-          <br />
-          <input id="classCode" name="classCode" />
-
         </label>
         <br />
-        <label for="isLiveUrl">
-          Deployed URL site:
-          <br />
-          <input id="isLiveUrl" name="isLiveUrl" />
+        <input id="projectClassCode" name="projectClassCode" required />
+        <br />
 
+        <label for="projectGithubUrl">
+          GitHub Repository URL:
         </label>
         <br />
+        <input type="url" id="projectGithubUrl" name="projectGithubUrl" required />
+        <br />
+
+        <fieldset>
+          <legend>Is the project currently deployed?</legend>
+          <input type="radio" id="isLiveYes" name="IsLiveStatus"
+            value="true" onChange={handleDeployedChange} checked={isDeployed === true} required />
+          <label for="isLiveYes">Yes</label>
+          <input type="radio" id="isLiveNo" name="IsLiveStatus"
+            value="false" onChange={handleDeployedChange} checked={isDeployed === false} required />
+          <label for="isLiveYes">No</label>
+          <br />
+          { isDeployed ?
+            <>
+              <label for="projectDeploymentUrl">
+                Deployed URL:
+              </label>
+              <br />
+              <input type="url" id="projectDeploymentUrl" name="projectDeploymentUrl" />
+            </>
+          : null }
+        </fieldset>
+        <br />
+
         <label for="projectTags">
-          Framework/libraries/tools used:
-          <br />
-          <input id="projectTags" name="projectTags" />
-
+          Framework/Libraries/Tools Used (Comma Separated):
         </label>
         <br />
-        <section for="projectTags">
-          Framework/libraries/tools checkbox:
-          <br />
-          <section id="checkboxLabels">
-
-            <label>
-              <input id="projectTags" name="projectTags" type="checkbox" value="html" defaultChecked="false" />
-              html
-            </label>
-            <label>
-              <input id="projectTags" name="projectTags" type="checkbox" value="react" />
-              react
-            </label>
-            <label>
-              <input id="projectTags" name="projectTags" type="checkbox" value="express" />
-              express
-            </label>
-          </section>
-
-        </section>
+        <input id="projectTags" name="projectTags" />
         <br />
-        <label for="myfile">
-          Select files/Upload images:
-          <input id="file" type="file" name="myfile" multiple />
-        </label>
+        <br />
 
+        <fieldset>
+        {/* 
+        "postedBy": "",                                   // Required, not shown, attach the user's id
+        */}
+          <legend>This will eventually be handled outside of the form:</legend>
+          <label for="projectPostedBy">
+            Posted By:
+          </label>
+          <br />
+          <input id="projectPostedBy" name="projectPostedBy" required />
+          <br />
+        </fieldset>
 
-        <input id="submitButton" type="submit" />
+        <fieldset>
+        {/* 
+        "upvotedBy": [],                                  // Shouldn't be set by the front end
+        "approved": false,                                // Shouldn't be set by the front end
+        "upvotes": 0,                                     // Shouldn't be set by the front end 
+        */}
+          <legend>This stuff should be handled server-side:</legend>
+          <label for="projectUpvotedBy">
+            Upvoted By (Comma Separated):
+          </label>
+          <br />
+          <input id="projectUpvotedBy" name="projectUpvotedBy" required />
+          <br />
+
+          <fieldset>
+            <legend>Is the project approved?</legend>
+            <input type="radio" id="isApprovedYes" name="projectApprovedStatus"
+              value="true" onChange={handleApprovedChange} checked={isApproved === true} required />
+            <label for="isApprovedYes">Yes</label>
+            <input type="radio" id="isApprovedNo" name="projectApprovedStatus"
+              value="false" onChange={handleApprovedChange} checked={isApproved === false} required />
+            <label for="isApprovedNo">No</label>
+          </fieldset>
+
+          <label for="projectUpvotes">
+            Upvotes:
+          </label>
+          <br />
+          <input type="number" id="projectUpvotes" name="projectUpvotes" required />
+          <br />
+
+        </fieldset>
+
+        <input id="submitButton" type="submit"></input>
+        <br /><br />
       </form>
     </div>
   );
