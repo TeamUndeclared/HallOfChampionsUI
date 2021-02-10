4a3b36d88cd5680e52d1be7b789355d93c8a03ea
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-02-10 12:09:20 -0800

    Oopise. Fixed code for main App, and added in documentation for browser router implementation.

--- src/Components/Hallway/Hallway.js
+++ src/Components/Hallway/Hallway.js
@@ -9,6 +9,14 @@ function Hallway() {
   return (
     <div id="hallway" className="Hallway">
       <Form />
+      {/* 
+        Or: 
+        <Main /> // A component with a collection of cases, min/max 8, preset 4 by default
+        <Case type="classLevel" filter="SD400" /> // This takes *something* to ask Matt (our API) for a certain set of data
+        <Admin /> // A special "case" with admin "projects"
+        <Form /> // Accessible by the Hamburger menu. This shows the form in a modal.
+        <Project id="2539867hfg3" /> // A.k.a "singleView", this pulls a simgle project and displays it (in a modal or whatever)
+      */}
     </div>
   );
 }
