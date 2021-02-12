af16b2374cb234bc9e4e36ef5dd5f49754120408
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-11 18:17:29 -0800

    admin start and logos

---
+++ src/Components/Admin/Admin.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Admin() {
+
+
+  return (
+    <>
+      
+      <div id='admin-header'>
+        <h1> IM AN ADMIN CPANEL</h1>
+      <p>this will be a table of all projects listing name, posted by, posed on dated, githuburl, aprovedstatus(conditional aprove btn), delete btn </p>
+      </div>
+      <div id='project-list'>
+        <p>get adminprojects call and display</p>
+      </div>
+      
+      </>
+  );
+}
+
+export default Admin;
